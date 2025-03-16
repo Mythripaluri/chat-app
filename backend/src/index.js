2ea61b929d3c9ef7b0d061f46d9fc2236ac4d3c0
Mythripaluri <mythrip737@gmail.com>
2025-03-16 11:20:20 +0530

    Database Setup, Signup, Login and Logout and UpdateProfile

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,21 @@
 import express from 'express';
-import dotenv from "dotenv"
-import {connectDB} from './lib/db.js'
+import dotenv from "dotenv";
+import {connectDB} from "./lib/db.js";
 import authRoutes from './routes/auth.route.js';
+import cookieParser from "cookie-parser";
 
 dotenv.config()
-const app = express();
+const app=express();
 
-const PORT = process.env.PORT 
+const PORT=process.env.PORT 
 
 app.use(express.json());
+
+app.use(cookieParser());  // parse cookies
+
 app.use("/api/auth",authRoutes)
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(5001,()=>{
+    console.log('Server is running on port:'+PORT)
     connectDB();
-});
+})
